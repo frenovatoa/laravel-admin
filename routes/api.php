146da47bbd4b4afd514e6345a3a36049fba9a806
felipe_renovato <frenovatoa@outlook.com>
2024-05-31 09:03:30 -0600

    Get Authenticated User.

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,7 @@ use App\Http\Controllers\AuthController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [AuthController::class, 'user']);
+});
