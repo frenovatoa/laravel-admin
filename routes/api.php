b0f73ff4028f2ad8f5b3cd54411a5bdf408ef206
felipe_renovato <frenovatoa@outlook.com>
2024-06-02 17:31:28 -0600

    Roles & Permissions.

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,4 +32,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('users/password', [AuthController::class, 'updatePassword']);
 
     Route::apiResource('users', UserController::class);
+    Route::apiResource('roles', RoleController::class);
+    Route::get('permissions', [PermissionController::class, 'index']);
 });
