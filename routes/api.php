2275f0913e2645062b47d9b07680b555977d6733
felipe_renovato <frenovatoa@outlook.com>
2024-07-03 16:19:31 -0600

    Uploading Images.

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,4 +37,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('roles', RoleController::class);
     Route::apiResource('products', ProductController::class);
     Route::get('permissions', [PermissionController::class, 'index']);
+    Route::post('upload', [ImageController::class, 'upload']);
 });
