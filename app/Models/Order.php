dd68fca36d1067a35a5238f9fdd6bf3b21a2dd54
felipe_renovato <frenovatoa@outlook.com>
2024-07-03 20:16:49 -0600

    Accessor Attributes for Full Name & Total Price.

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -23,6 +23,11 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Order whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Order whereLastName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Order whereUpdatedAt($value)
+ * @property-read mixed $name
+ * @property-read mixed $total
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\OrderItem> $orderItems
+ * @property-read int|null $order_items_count
+ * @method static \Database\Factories\OrderFactory factory($count = null, $state = [])
  * @mixin \Eloquent
  */
 class Order extends Model
@@ -31,5 +36,16 @@ class Order extends Model
 
     function orderItems() {
         return $this->hasMany(OrderItem::class);
+    }
+
+    // Accessor in laravel transforms an Eloquent attribute value that is been accessed. To create an accessor, there is a method named get{AttributeName}Attribute which returns the expected thing that you want when you access the attribute.
+    // For more information, visit: https://dikshantraj2001.medium.com/accessors-and-mutators-in-laravel-1d4114dd3bc0
+
+    public function getNameAttribute() {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
+    public function getTotalAttribute() {
+        return $this->orderItems->sum(fn(OrderItem $orderItem) => $orderItem->price * $orderItem->quantity);
     }
 }
